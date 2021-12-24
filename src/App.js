d9db980377400dac6e3b358cc0f420f9e4b0f50f
aniruddhajana239 <aniruddhajana239@gmail.com>
2021-12-24 23:16:06 +0530

    scss update

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ import Projects from "./components/Projects";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
-import "./scss/header.scss";
-import "./scss/home.scss";
+import "./scss/style.scss";
 
 const App = () => {
   return (
@@ -19,8 +18,8 @@ const App = () => {
         <Route path="Services" element={<Services />} />
         <Route path="Projects" element={<Projects />} />
         <Route path="About" element={<About />} />
-        <Route path="Contact" element={<Contact/>}/>
-        <Route path="Error" element={<Error/>}/>
+        <Route path="Contact" element={<Contact />} />
+        <Route path="Error" element={<Error />} />
       </Routes>
     </>
   )
